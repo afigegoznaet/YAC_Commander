18cd5a7238caed5d8a74b56cd2877d06fe9fb216
afigegoznaet <enrabiar@gmail.com>
2019-03-17 19:17:45 +0200

    Some fixes

--- Models/OrderedFileSystemModel.cpp
+++ Models/OrderedFileSystemModel.cpp
@@ -1,99 +1,101 @@
 #include "OrderedFileSystemModel.hpp"
+#include "../Dialogs/FileProgressDialog.hpp"
 #include <QDebug>
 #include <QFileSystemModel>
-#include "../Dialogs/FileProgressDialog.hpp"
 
 OrderedFileSystemModel::OrderedFileSystemModel(QObject *parent)
 	: QSortFilterProxyModel(parent) {}
 
 bool OrderedFileSystemModel::lessThan(const QModelIndex &left,
 									  const QModelIndex &right) const {
-	const QFileInfo &leftFile = sourceModel()->fileInfo(left);
-	const QFileInfo &rightFile = sourceModel()->fileInfo(right);
-	if (leftFile.isDir() ^ rightFile.isDir()) {
-		if (this->order)
-			return !leftFile.isDir();
+  const QFileInfo &leftFile = sourceModel()->fileInfo(left);
+  const QFileInfo &rightFile = sourceModel()->fileInfo(right);
+  if (leftFile.isDir() ^ rightFile.isDir()) {
+	if (this->order)
+	  return !leftFile.isDir();
 
-		return leftFile.isDir(); // will it be optimized
-	}
-	bool res = false;
-	if (!leftFile.fileName().compare(".."))
-		return order == Qt::AscendingOrder;
-	if (!rightFile.fileName().compare(".."))
-		return order != Qt::AscendingOrder;
-	switch (column) {
-	case 0:
-		res = leftFile.fileName().compare(rightFile.fileName()) < 0;
-		break;
-	case 2:
-		res = leftFile.suffix().compare(rightFile.suffix()) < 0;
-		break;
-	case 3:
-		res = leftFile.created().toTime_t() < rightFile.created().toTime_t();
-		break;
-	case 1:
-		res = leftFile.size() < rightFile.size();
-		break;
-	}
+	return leftFile.isDir(); // will it be optimized
+  }
+  bool res = false;
+  if (!leftFile.fileName().compare(".."))
+	return order == Qt::AscendingOrder;
+  if (!rightFile.fileName().compare(".."))
+	return order != Qt::AscendingOrder;
+  switch (column) {
+  case 0:
+	res = leftFile.fileName().compare(rightFile.fileName()) < 0;
+	break;
+  case 2:
+	if (leftFile.isDir())
+	  res = leftFile.fileName().compare(rightFile.fileName()) < 0;
+	else
+	  res = leftFile.suffix().compare(rightFile.suffix()) < 0;
+	break;
+  case 3:
+	res = leftFile.created().toTime_t() < rightFile.created().toTime_t();
+	break;
+  case 1:
+	res = leftFile.size() < rightFile.size();
+	break;
+  }
 
-	return res;
+  return res;
 }
 
 void OrderedFileSystemModel::sort(int column, Qt::SortOrder order) {
-	this->column = column;
-	this->order = order;
-	QSortFilterProxyModel::sort(column, order);
-	emit directoryLoaded("");
+  this->column = column;
+  this->order = order;
+  QSortFilterProxyModel::sort(column, order);
+  emit directoryLoaded("");
 }
 
 QFileSystemModel *OrderedFileSystemModel::sourceModel() const {
-	return dynamic_cast<QFileSystemModel *>(
-		QSortFilterProxyModel::sourceModel());
+  return dynamic_cast<QFileSystemModel *>(QSortFilterProxyModel::sourceModel());
 }
 
 bool OrderedFileSystemModel::dropMimeData(const QMimeData *data,
 										  Qt::DropAction action, int row, int,
 										  const QModelIndex &) {
-	auto uriList = data->urls();
-	QFileInfoList itemsToMove;
+  auto uriList = data->urls();
+  QFileInfoList itemsToMove;
 
-	auto dir = rootPath();
+  auto dir = rootPath();
 
-	for (const auto &uri : uriList)
-		itemsToMove << uri.toLocalFile();
-	if (0 == row) // selection is ".."
-		dir.truncate(dir.lastIndexOf('/'));
-	emit setFileAction(itemsToMove, dir, action);
-	return true;
+  for (const auto &uri : uriList)
+	itemsToMove << uri.toLocalFile();
+  if (0 == row) // selection is ".."
+	dir.truncate(dir.lastIndexOf('/'));
+  emit setFileAction(itemsToMove, dir, action);
+  return true;
 }
 
 Qt::ItemFlags OrderedFileSystemModel::flags(const QModelIndex &index) const {
-	Qt::ItemFlags defaultFlags = QSortFilterProxyModel::flags(index);
-	if (0 == index.column())
-		defaultFlags |= Qt::ItemIsEditable;
-	if (index.isValid())
-		return Qt::ItemIsDragEnabled | Qt::ItemIsDropEnabled | defaultFlags;
+  Qt::ItemFlags defaultFlags = QSortFilterProxyModel::flags(index);
+  if (0 == index.column())
+	defaultFlags |= Qt::ItemIsEditable;
+  if (index.isValid())
+	return Qt::ItemIsDragEnabled | Qt::ItemIsDropEnabled | defaultFlags;
 
-	return Qt::ItemIsDropEnabled | defaultFlags;
+  return Qt::ItemIsDropEnabled | defaultFlags;
 }
 
 QString OrderedFileSystemModel::rootPath() {
-	return sourceModel()->rootDirectory().absolutePath();
+  return sourceModel()->rootDirectory().absolutePath();
 }
 
 QModelIndex OrderedFileSystemModel::setRootPath(const QString &rootPath) const {
-	return mapFromSource(sourceModel()->setRootPath(rootPath));
+  return mapFromSource(sourceModel()->setRootPath(rootPath));
 }
 QModelIndex OrderedFileSystemModel::getRootIndex() const {
-	return mapFromSource(sourceModel()->index(sourceModel()->rootPath()));
+  return mapFromSource(sourceModel()->index(sourceModel()->rootPath()));
 }
 QModelIndex OrderedFileSystemModel::getSourceRootIndex() const {
-	return (sourceModel()->index(sourceModel()->rootPath()));
+  return (sourceModel()->index(sourceModel()->rootPath()));
 }
 QFileInfo OrderedFileSystemModel::fileInfo(QModelIndex index) const {
-	return sourceModel()->fileInfo(mapToSource(index));
+  return sourceModel()->fileInfo(mapToSource(index));
 }
 
 void OrderedFileSystemModel::setFilter(QDir::Filters filt) const {
-	sourceModel()->setFilter(filt);
+  sourceModel()->setFilter(filt);
 }
--- Views/FileTableView.hpp
+++ Views/FileTableView.hpp
@@ -4,8 +4,8 @@
 
 //#include <QWidget>
 //#include <QListView>
-#include <QTableView>
 #include <QDir>
+#include <QTableView>
 #include <QTimer>
 
 class TableItemDelegate;
@@ -15,79 +15,77 @@ class ItemContextMenu;
 class FileTabSelector;
 
 class FileTableView : public QTableView {
-	Q_OBJECT
+  Q_OBJECT
 
-	enum Action { PLUS, MINUS, ASTERISK };
+  enum Action { PLUS, MINUS, ASTERISK };
 
 public:
-	explicit FileTableView(const QDir &directory, QWidget *parent = 0);
-	FileTableView(QWidget *parent) : FileTableView(QDir::homePath(), parent) {}
-	//~FileTableView(){delete prevSelection;}
+  explicit FileTableView(const QDir &directory, QWidget *parent = 0);
+  FileTableView(QWidget *parent) : FileTableView(QDir::homePath(), parent) {}
+  //~FileTableView(){delete prevSelection;}
 
-	void init();
+  void init();
 
-	QFileInfoList getSelectedFiles();
-	void cdTo(const QString &);
-	TableItemDelegate *itemDelegate() const {
-		// return qobject_cast<TableItemDelegate *>(QTableView::itemDelegate());
-		return (TableItemDelegate *)QTableView::itemDelegate();
-	}
-	OrderedFileSystemModel *getModel() { return model; }
-	QModelIndexList getSelectedIndexes();
-	QString getDirectory();
-	// QLabel *getLabel() { return infoLabel; }
-	void setTabOrderIndex(int index) { this->index = index; }
-	void setLabel(QLabel *infoLabel) { this->infoLabel = infoLabel; }
+  QFileInfoList getSelectedFiles();
+  void cdTo(const QString &);
+  TableItemDelegate *itemDelegate() const {
+	// return qobject_cast<TableItemDelegate *>(QTableView::itemDelegate());
+	return (TableItemDelegate *)QTableView::itemDelegate();
+  }
+  OrderedFileSystemModel *getModel() { return model; }
+  QModelIndexList getSelectedIndexes();
+  QString getDirectory();
+  // QLabel *getLabel() { return infoLabel; }
+  void setTabOrderIndex(int index) { this->index = index; }
+  void setLabel(QLabel *infoLabel) { this->infoLabel = infoLabel; }
 
-
-	void goToFile(QString &fullFilePath);
-	void openEditor(QModelIndex &index);
-	void deleteSelectedFiles();
-	void showHidden(bool show);
-	OrderedFileSystemModel *getModel() const { return model; }
+  void goToFile(const QString &fullFilePath);
+  void openEditor(QModelIndex &index);
+  void deleteSelectedFiles();
+  void showHidden(bool show);
+  OrderedFileSystemModel *getModel() const { return model; }
 
 signals:
-	// void activated(const QModelIndex &index);
-	void dirChanged(const QString dirName, int index);
-	void focusEvent(bool);
-	void setFileAction(QFileInfoList, QString, Qt::DropAction);
-	void contextMenuRequested(QPoint);
-
+  // void activated(const QModelIndex &index);
+  void dirChanged(const QString dirName, int index);
+  void focusEvent(bool);
+  void setFileAction(QFileInfoList, QString, Qt::DropAction);
+  void contextMenuRequested(QPoint);
 
 public slots:
-	void on_doubleClicked(const QModelIndex &index);
-	void setCurrentSelection(const QString &);
-	void headerClicked(int section);
-	void rowsRemoved(const QModelIndex &, int, int);
-	void rowsInserted(const QModelIndex &parent, int first, int) override;
-	void updateInfo();
-	void openContextMenu(QPoint);
-	void commitNewName(QWidget *editor);
+  void on_doubleClicked(const QModelIndex &index);
+  void setCurrentSelection(const QString &);
+  void headerClicked(int section);
+  void rowsRemoved(const QModelIndex &, int, int);
+  void rowsInserted(const QModelIndex &parent, int first, int) override;
+  void updateInfo();
+  void openContextMenu(QPoint);
+  void commitNewName(QWidget *editor);
 
 protected:
-	void keyPressEvent(QKeyEvent *event) override;
-	void chDir(const QModelIndex &index, bool in_out);
-	void focusInEvent(QFocusEvent *event) override;
-	void focusOutEvent(QFocusEvent *event) override;
-	bool isCurrent() const;
+  void keyPressEvent(QKeyEvent *event) override;
+  void chDir(const QModelIndex &index, bool in_out);
+  void focusInEvent(QFocusEvent *event) override;
+  void focusOutEvent(QFocusEvent *event) override;
+  bool isCurrent() const;
 
 private:
-	int index{};
-	int prevRow = -1;
-	bool editorIsOpen = false;
-	bool slowDoubleClick = false;
-	QLabel *infoLabel = nullptr;
-	TableItemDelegate *delegate = nullptr;
-	ItemContextMenu *menu;
-	QString directory;
-	QTimer slowDoubleClickTimer;
-	OrderedFileSystemModel *model{};
-	FileTabSelector *parent = nullptr;
+  int index{};
+  int prevRow = -1;
+  bool editorIsOpen = false;
+  bool slowDoubleClick = false;
+  QLabel *infoLabel = nullptr;
+  TableItemDelegate *delegate = nullptr;
+  ItemContextMenu *menu;
+  QString directory;
+  QTimer slowDoubleClickTimer;
+  OrderedFileSystemModel *model{};
+  FileTabSelector *parent = nullptr;
 
-	void mousePressEvent(QMouseEvent *event) override;
-	void mouseReleaseEvent(QMouseEvent *event) override;
-	void queryDialog(QString &filter, Action act);
-	void setSelectionAction(Action act);
+  void mousePressEvent(QMouseEvent *event) override;
+  void mouseReleaseEvent(QMouseEvent *event) override;
+  void queryDialog(QString &filter, Action act);
+  void setSelectionAction(Action act);
 };
 
 #endif // TAB_H
